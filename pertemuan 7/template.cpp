04dd2ed6fc8fb6b2a5baeb01e158152e36118503
kartikaaadn <kartikaadindakd@gmail.com>
2024-03-27 09:47:40 +0700

    tempalte2

---
+++ pertemuan 7/template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+tempalte <typename T>
+
+T jumlah(T a, T b){
+    return a + b;
+}
+
+int main(){
+    int a = 10, b = 20;
+    double x = 2.5, y = 7.5,
+
+    cout jumlah(a,b) << endl;
+    cout jumlah(x, y) << endl;
+}
